792d1cdb7c7055b075196a7c7873deac14978a47
Tanveer Ansari <tanveeransari5663@gmail.com>
2025-11-04 22:49:03 +0530

    Update JWT secret in authMiddleware
    Replaced JWT secret with a hardcoded value for testing.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -15,7 +15,7 @@ export const protect = async (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, "avaedefe@#$##");
     // attach user info to request (without password)
     const user = await User.findById(decoded.id).select('-password');
     if (!user) return res.status(401).json({ message: 'User not found' });
