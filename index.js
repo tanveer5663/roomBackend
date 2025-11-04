c63287043c2628b85b64065296cd962ca718ca03
Tanveer <tanveeransari5663@gmail.com>
2025-11-04 23:00:33 +0530

    new

--- index.js
+++ index.js
@@ -7,36 +7,35 @@ import authRoutes from "./routes/authRoutes.js";
 import protectedRoutes from "./routes/protectedRoutes.js";
 import propertyRoutes from "./routes/Propertyroutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
+import connectDB from "./db.js";
 
 dotenv.config();
 
 const app = express();
+connectDB();
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Example routes
-app.get("/", (req, res) => {
-  res.send("Welcome to my simple Node.js API 🚀");
-});
-
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Node.js API!" });
-});
+app.use("/api/auth", authRoutes);
+app.use("/api", protectedRoutes);
+app.use("/api/properties", propertyRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
-const MONGO =
-  process.env.MONGO_URI ||
-  "mongodb+srv://yadavbhavesh:12345@rooms.xeajzyr.mongodb.net/Rooms-Data?retryWrites=true&w=majority&appName=Rooms";
+// const MONGO =
+//   process.env.MONGO_URI ||
+//   "mongodb+srv://yadavbhavesh:12345@rooms.xeajzyr.mongodb.net/Rooms-Data?retryWrites=true&w=majority&appName=Rooms";
 
-mongoose
-  .connect(MONGO)
-  .then(() => {
-    console.log("✅ Connected to MongoDB");
-    //app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => {
-    console.error("❌ Failed to connect to MongoDB:", err);
-  });
- 
+// mongoose
+//   .connect(MONGO)
+//   .then(() => {
+//     console.log("✅ Connected to MongoDB");
+//     //app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//   })
+//   .catch((err) => {
+//     console.error("❌ Failed to connect to MongoDB:", err);
+//   });
+
 export default app;
